d9e9a62a9a13109794a6d12d61544a75f046fcdc
Kippy <not@telling.ever>
2023-02-24 02:31:46 +0100

    flatten public assets

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="app">
       <div className="hero">
-        <Needle path="assets/sceneRoot.glb"/>
+        <Needle path="sceneRoot.glb"/>
         <div className="hero-fade"/>
       </div>
       <ThemeProvider theme={tosh}>
