ec651d6524d9ae77e4c0898a617bff19126748e0
Kippy <not@telling.ever>
2023-02-26 19:04:38 +0100

    refactor needle component ; remove concept image ;  update 3D scene

--- src/Needle.tsx
+++ src/Needle.tsx
@@ -5,23 +5,24 @@ type NeedleProps = {
   path: string
 }
 
+
 export const Needle = ({ path }: NeedleProps) => 
 {
-  const parentRef = React.useRef(null);
-
-  let element = document.createElement('needle-engine');
-  element.setAttribute("src", path);
-  element.setAttribute("loadfinished", "");
+  const parentRef = React.useRef<HTMLDivElement | null>(null);
 
   React.useEffect(() => {
-    console.log("Needle: useEffect"); 
+    const element = document.createElement('needle-engine');
+    element.setAttribute("src", path);
+    /* element.addEventListener("loadfinished", () => console.log("Loaded")); */
+
     if(parentRef.current)
-    {
-      const parentElem = (parentRef.current as HTMLElement);
-      if(parentElem && element.parentNode == null)
-        parentElem.appendChild(element);
+        parentRef.current.appendChild(element);
+
+    return () => {
+      if(element.parentNode)
+        element.parentNode.removeChild(element);
     }
-  });
+  }, []);
 
   return (
     <div ref={parentRef}/>
